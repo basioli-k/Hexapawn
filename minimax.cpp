3163cf033df4cc3cce842b94b9ec2b8a9fd0ac93
iavirovic <ivan.avirovic@gmail.com>
2020-12-28 17:38:59 +0100

    dodavanje heuristika

--- minimax.cpp
+++ minimax.cpp
@@ -4,11 +4,49 @@
 
 helpers helper;
 
-int h(vector<string> &board, player &white, player &black)
+// Heuristika koja broji koliko koji igrac ima pijuna.
+int h1(vector<string> &board, player &white, player &black)
 {
     int w=white.pawns.size(),b=black.pawns.size();
     return w-b;   
 }
+
+// Heuristika koja gleda koliko su pijuni od svakog igraca 'razmaknuti'. Neka sahovska intuicija nam govori da bi bilo bolje da su svi pijuni medusobno blizu kako bi se branili.
+int h2(vector<string> &board, player &white, player &black)
+{
+    int minWhite = 1e9, maxWhite = -1, minBlack = 1e9, maxBlack = -1;
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[0].size(); j++) {
+            if (board[i][j] == 'w') {
+                minWhite = min(minWhite, i);
+                maxWhite = max(maxWhite, i);
+            }
+            if (board[i][j] == 'b') {
+                minBlack = min(minBlack, i);
+                maxBlack = max(maxBlack, i);
+            }
+        }
+    }
+    return (maxBlack - minBlack) - (maxWhite - minWhite);
+}
+
+// Heuristika koja gleda koliko je blizu rubu ploce najudaljeniji pijun
+int h3(vector<string> &board, player &white, player &black)
+{
+    int minWhite = 1e9, maxBlack = -1;
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[0].size(); j++) {
+            if (board[i][j] == 'w') {
+                minWhite = min(minWhite, i);
+            }
+            if (board[i][j] == 'b') {
+                maxBlack = max(maxBlack, i);
+            }
+        }
+    }
+    return (board.size() - 1 - maxBlack) - minWhite;
+}
+
 int minimax::minimaxAlg(vector<string> &board, player &white, player &black, bool whitePlays, int depth,int alpha,int beta,bool &noLegalMoves){
     
     if(helper.evaluateBoard(board) != 0){
@@ -16,7 +54,7 @@ int minimax::minimaxAlg(vector<string> &board, player &white, player &black, boo
     }
     if(white.pawns.size()==0 && black.pawns.size()!=0) return INT_MIN;
     if(black.pawns.size() == 0 && white.pawns.size() != 0) return INT_MAX;
-    if(depth>=min(10,helper.intlog(3*board[0].size(),1e9))) return h(board,white,black);
+    if(depth>=min(10,helper.intlog(3*board[0].size(),1e9))) return h2(board,white,black);
     vector<string> bestBoard;
     player bestWhite=player(true),bestBlack=player(false);
     noLegalMoves=true;
